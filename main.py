ccb5a3f3ac832b4a84b21628d9fc7075538ae7cd
Shokhrukh <shokhablessed@gmail.com>
2026-06-05 20:22:44 +0500

    Add algorithm selection menu

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from solver import solve_bfs
+from solver import solve_bfs, solve_dfs
 from renderer import print_maze, visualize_path
 from loader import load_maze
 
 maze = load_maze("maze.txt")
 
+print("Choose algorithm:")
+print("1. BFS")
+print("2. DFS")
+
+choice = input("Enter choice: ")
+
 print("Original maze:")
 print_maze(maze)
 
-path = solve_bfs(maze)
+if choice == "1":
+    algorithm = "BFS"
+    path = solve_bfs(maze)
+
+elif choice == "2":
+    algorithm = "DFS"
+    path = solve_dfs(maze)
+
+else:
+    print("Invalid choice")
+    exit()
+
+print(f"\nAlgorithm: {algorithm}")
 
 print("\nPath:")
 print(path)
