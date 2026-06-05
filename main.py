f579a539d4268ae06532de14ec0134228226dabd
Shokhrukh <shokhablessed@gmail.com>
2026-06-05 19:58:25 +0500

    Add maze solver demonstration

---
+++ main.py
@@ -0,0 +1,21 @@
+from solver import solve_bfs
+from renderer import print_maze, visualize_path
+
+maze = [
+    ["S", ".", "."],
+    ["#", "#", "."],
+    [".", ".", "E"]
+]
+
+print("Original maze:")
+print_maze(maze)
+
+path = solve_bfs(maze)
+
+print("\nPath:")
+print(path)
+
+solved = visualize_path(maze, path)
+
+print("\nSolved maze:")
+print_maze(solved)
